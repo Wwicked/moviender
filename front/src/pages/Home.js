c80e22a67540838fe2744dc53e4bf1bdeca0ba13
Robin <robertmokanek@gmail.com>
2023-04-24 14:06:28 +0200

    Temporary home page

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import UserService from "../services/user.service";
 
 const Home = () => {
+    useEffect(() => {
+        UserService.read().then((res) => {
+            console.log(`Response: ${JSON.stringify(res)}`);
+        });
+    }, []);
+
     return <div>Home</div>;
 };
 
