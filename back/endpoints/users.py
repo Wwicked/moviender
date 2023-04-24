6eeb0ac3ad96769034bd92ca4df6a129834e976e
Robin <robertmokanek@gmail.com>
2023-04-24 14:05:50 +0200

    Temporary user getterthrough API

--- back/endpoints/users.py
+++ back/endpoints/users.py
@@ -1,8 +1,17 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from models import User
 
 users_blueprint = Blueprint("users", __name__)
 
 
-@users_blueprint.route("/<int:id>", methods=["GET"])
-def get_by_id(id):
-    return jsonify({"message": f"Input id:{id}"})
+@users_blueprint.route("/", methods=["GET"])
+@jwt_required()
+def get_user():
+    user = User.query.filter_by(token=get_jwt_identity()).first_or_404()
+    res = user.to_dict()
+
+    res.pop("token")
+    res.pop("password")
+
+    return jsonify(res), 200
