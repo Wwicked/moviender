702cf2a74fd6fa47a745f78305a03c0e6348b9a3
Robin <robertmokanek@gmail.com>
2023-04-24 20:54:40 +0200

    Prettify api.js and token updating function

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -2,14 +2,20 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const API_URL = "http://localhost:5000";
+const token = Cookies.get("access_token");
 const api = axios.create({
     baseURL: API_URL,
 });
-const token = Cookies.get("access_token");
 
 if (token) {
     api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-    console.log(`Setting header token to: ${token}`);
 }
 
-export default api;
+const updateAuthCookiesAndHeader = (access_token, refresh_token) => {
+    Cookies.set("access_token", access_token);
+    Cookies.set("refresh_token", refresh_token);
+
+    api.defaults.headers.common["Authorization"] = `Bearer ${access_token}`;
+};
+
+export { api, updateAuthCookiesAndHeader };
