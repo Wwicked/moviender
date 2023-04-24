e316c6a55f23c61d7d44eb6bd5827376e76f9975
Robin <robertmokanek@gmail.com>
2023-04-24 20:55:32 +0200

    Loading user as first thing in main app

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,21 @@
-import React from "react";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import PageNotFound from "./pages/PageNotFound";
 import { Routes, Route, Navigate, Outlet } from "react-router-dom";
 import { useCookies } from "react-cookie";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { SET_USER } from "./reducers/types";
+import UserService from "./services/user.service";
+import Spinner from "react-bootstrap/Spinner";
+import Container from "react-bootstrap/Container";
 
 const ProtectedRoute = ({ redirectPath = "/login" }) => {
     const [cookies] = useCookies(["user"]);
 
-    if (!cookies.access_token) {
+    if (!cookies?.access_token) {
         return <Navigate to={redirectPath} replace />;
     }
 
@@ -17,6 +23,48 @@ const ProtectedRoute = ({ redirectPath = "/login" }) => {
 };
 
 const App = () => {
+    const { user } = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(false);
+    const [cookies] = useCookies(["user"]);
+
+    useEffect(() => {
+        setLoading(true);
+
+        if (user || !cookies?.access_token) {
+            setLoading(false);
+            return;
+        }
+
+        UserService.read()
+            .then((res) => {
+                dispatch({
+                    type: SET_USER,
+                    payload: res.data,
+                });
+
+                setLoading(false);
+            })
+            .catch((err) => {
+                setLoading(false);
+            });
+    }, [dispatch, user, cookies?.access_token]);
+
+    if (loading) {
+        return (
+            <Container
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+                className="my-5"
+            >
+                <Spinner />
+            </Container>
+        );
+    }
+
     return (
         <Routes>
             <Route exact path="/login" element={<Login />} />
