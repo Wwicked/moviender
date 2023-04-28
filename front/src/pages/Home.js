d5e9717cec633e3d3c559230f33c6adb8bcfce83
Robin <robertmokanek@gmail.com>
2023-04-28 18:40:50 +0200

    Two cards instead of one

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import MovieCard from "../components/MovieCard/MovieCard";
 import InfoModal from "../components/MovieCard/InfoModal";
 import UserService from "../services/user.service";
+import "./Home.css";
 
 const movie = {
     id: 1,
@@ -24,6 +25,53 @@ const movie = {
         },
     ],
     genres: ["Crime", "Drama", "Crime"],
+    cast: [
+        {
+            real: "Al Pacino",
+            movie: "Michael Corleone",
+        },
+        {
+            real: "Marlon Brando",
+            movie: "Vito Corleone",
+        },
+        {
+            real: "James Caan",
+            movie: "Sonny Corleone",
+        },
+        {
+            real: "Diane Keaton",
+            movie: "Kay Adams-Corleone",
+        },
+        {
+            real: "Talia Shire",
+            movie: "Connie Corleone",
+        },
+        {
+            real: "John Cazale",
+            movie: "Fredo Corleone",
+        },
+    ],
+};
+
+const movieUnder = {
+    id: 1,
+    title: "2 The Godfather",
+    description:
+        "Don Vito Corleone, head of a mafia family, decides to hand over his empire to his youngest son Michael. However, his decision unintentionally puts the lives of his loved ones in grave danger.",
+    year: 2001,
+    duration: 192,
+    images: [
+        "https://www.themoviedb.org/t/p/w1280/rPdtLWNsZmAtoZl9PK7S2wE3qiS.jpg",
+        "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+    ],
+    funFacts: [
+        {
+            header: "The horse's head was real! ",
+            content:
+                "...as were actor John Marley’s screams. A fake horse head was used in rehearsals, but when the cameras were actually rolling, Coppola replaced it with the real thing, much to Marley’s surprise.",
+        },
+    ],
+    genres: ["Crime", "Drama"],
     cast: [
         {
             real: "Al Pacino",
@@ -119,17 +167,34 @@ const Home = () => {
             </Container>
 
             <Container className="d-flex justify-content-center">
-                <MovieCard
-                    movie={movie}
-                    buttonsBlocked={buttonsBlocked}
-                    swipeBlocked={swipeBlocked}
-                    onSwipeLeft={handleSwipeLeft}
-                    onSwipeRight={handleSwipeRight}
-                    onLike={handleLike}
-                    onDislike={handleDislike}
-                    onWatchLater={handleWatchLater}
-                    onInfo={handleInfo}
-                />
+                <div className="under">
+                    <MovieCard
+                        movie={movieUnder}
+                        buttonsBlocked={buttonsBlocked}
+                        swipeBlocked={swipeBlocked}
+                        onSwipeLeft={handleSwipeLeft}
+                        onSwipeRight={handleSwipeRight}
+                        onLike={handleLike}
+                        onDislike={handleDislike}
+                        onWatchLater={handleWatchLater}
+                        onInfo={handleInfo}
+                    />
+                </div>
+
+                <div className="over">
+                    <MovieCard
+                        movie={movie}
+                        buttonsBlocked={buttonsBlocked}
+                        swipeBlocked={swipeBlocked}
+                        onSwipeLeft={handleSwipeLeft}
+                        onSwipeRight={handleSwipeRight}
+                        onLike={handleLike}
+                        onDislike={handleDislike}
+                        onWatchLater={handleWatchLater}
+                        onInfo={handleInfo}
+                    />
+                </div>
+
                 <InfoModal show={showModal} movie={modalData} onClose={handleModalClose} />
             </Container>
         </Container>
