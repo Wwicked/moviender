8b461c1dd3e77357deed40aac56d8d265a7538e6
Robin <robertmokanek@gmail.com>
2023-05-02 18:03:21 +0200

    Logging out from nav

--- front/src/components/Nav.js
+++ front/src/components/Nav.js
@@ -2,11 +2,18 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 const NavLoggedIn = () => {
     const { user } = useSelector((state) => state.user);
+    const [, , removeCookie] = useCookies(["user"]);
+
+    const onLogOut = () => {
+        removeCookie("access_token", { path: "/" });
+        removeCookie("refresh_token", { path: "/" });
+    };
 
     return (
         <Navbar bg="light" expand="lg">
@@ -15,22 +22,19 @@ const NavLoggedIn = () => {
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll>
-                        <Nav.Link href="#action1">Home</Nav.Link>
-                        <Nav.Link href="#action2">Link</Nav.Link>
-                        <NavDropdown title="Link" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="#" disabled>
-                            Link
+                        <Nav.Link href="#">
+                            <Link to="/admin/new" replace>
+                                New movie
+                            </Link>
                         </Nav.Link>
                     </Nav>
                     <Form className="d-flex gap-3">
                         <Navbar.Text>
                             Hello, <a href="#login">{user?.username}</a>
                         </Navbar.Text>
+                        <Nav.Link href="#" onClick={onLogOut}>
+                            Log out
+                        </Nav.Link>
                     </Form>
                 </Navbar.Collapse>
             </Container>
