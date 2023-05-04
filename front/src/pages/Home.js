e761e5d1e8de4c1925b23a2471abb8cd9069cb89
Robin <robertmokanek@gmail.com>
2023-05-04 11:52:39 +0200

    Loading movies from db instead of static ones on /

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,138 +1,71 @@
 import React, { useEffect, useState } from "react";
-import { Container, Spinner } from "react-bootstrap";
+import { Container, Spinner, Button } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import MovieCard from "../components/MovieCard/MovieCard";
 import InfoModal from "../components/MovieCard/InfoModal";
 import UserService from "../services/user.service";
+import MovieService from "../services/movie.service";
 import "./Home.css";
-
-const dummyMovieOne = {
-    id: 1,
-    title: "The Godfather",
-    description:
-        "Don Vito Corleone, head of a mafia family, decides to hand over his empire to his youngest son Michael. However, his decision unintentionally puts the lives of his loved ones in grave danger. Don Vito Corleone, head of a mafia family, decides to hand over his empire to his youngest son Michael. However, his decision unintentionally puts the lives of his loved ones in grave danger. Don Vito Corleone, head of a mafia family, decides to hand over his empire to his youngest son Michael. However, his decision unintentionally puts the lives of his loved ones in grave danger.",
-    release: 1972,
-    duration: 175,
-    images: [
-        "https://www.themoviedb.org/t/p/w1280/rPdtLWNsZmAtoZl9PK7S2wE3qiS.jpg",
-        "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
-    ],
-    fun_facts: [
-        {
-            header: "The horse's head was real! ",
-            content:
-                "...as were actor John Marley's screams. A fake horse head was used in rehearsals, but when the cameras were actually rolling, Coppola replaced it with the real thing, much to Marley's surprise.",
-        },
-    ],
-    genres: ["Crime", "Drama", "Crime"],
-    cast: [
-        {
-            real: "Al Pacino",
-            movie: "Michael Corleone",
-        },
-        {
-            real: "Marlon Brando",
-            movie: "Vito Corleone",
-        },
-        {
-            real: "James Caan",
-            movie: "Sonny Corleone",
-        },
-        {
-            real: "Diane Keaton",
-            movie: "Kay Adams-Corleone",
-        },
-        {
-            real: "Talia Shire",
-            movie: "Connie Corleone",
-        },
-        {
-            real: "John Cazale",
-            movie: "Fredo Corleone",
-        },
-    ],
-};
-
-const dummyMovieTwo = {
-    id: 1,
-    title: "2 The Godfather",
-    description:
-        "Don Vito Corleone, head of a mafia family, decides to hand over his empire to his youngest son Michael. However, his decision unintentionally puts the lives of his loved ones in grave danger.",
-    release: 2001,
-    duration: 192,
-    images: [
-        "https://www.themoviedb.org/t/p/w1280/rPdtLWNsZmAtoZl9PK7S2wE3qiS.jpg",
-        "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
-    ],
-    fun_facts: [
-        {
-            header: "The horse's head was real! ",
-            content:
-                "...as were actor John Marley's screams. A fake horse head was used in rehearsals, but when the cameras were actually rolling, Coppola replaced it with the real thing, much to Marley's surprise.",
-        },
-    ],
-    genres: ["Crime", "Drama"],
-    cast: [
-        {
-            real: "Al Pacino",
-            movie: "Michael Corleone",
-        },
-        {
-            real: "Marlon Brando",
-            movie: "Vito Corleone",
-        },
-        {
-            real: "James Caan",
-            movie: "Sonny Corleone",
-        },
-        {
-            real: "Diane Keaton",
-            movie: "Kay Adams-Corleone",
-        },
-        {
-            real: "Talia Shire",
-            movie: "Connie Corleone",
-        },
-        {
-            real: "John Cazale",
-            movie: "Fredo Corleone",
-        },
-    ],
-    video_id: "UaVTIH8mujA",
-};
+import EmptyCard from "../components/EmptyCard/EmptyCard";
 
 const Home = () => {
     const { user } = useSelector((state) => state.user);
 
-    const [movieOver, setMovieOver] = useState(null);
-    const [movieUnder, setMovieUnder] = useState(null);
+    const [movies, setMovies] = useState([]);
 
     const [showModal, setShowModal] = useState(false);
     const [modalData, setModalData] = useState(null);
 
     const [loading, setLoading] = useState(true);
+    const [failedToLoad, setFailedToLoad] = useState(false);
     const [clicked, setClicked] = useState(false);
 
     const [swipeDirection, setSwipeDirection] = useState(null);
 
     useEffect(() => {
-        setLoading(true);
-
-        setMovieOver(dummyMovieOne);
-        setMovieUnder(dummyMovieTwo);
-
-        setLoading(false);
+        loadFirstTwo();
     }, []);
+
+    const loadFirstTwo = () => {
+        setLoading(true);
+        setFailedToLoad(true);
+
+        MovieService.loadNext().then((res) => {
+            setMovies((prev) => [...prev, res.data]);
+            setLoading(false);
+            setFailedToLoad(false);
+        });
+
+        MovieService.loadNext().then((res) => {
+            setMovies((prev) => [...prev, res.data]);
+            setLoading(false);
+            setFailedToLoad(false);
+        });
+    };
+
+    const loadNextMovie = () => {
+        MovieService.loadNext()
+            .then((res) => {
+                setMovies((prev) => [...prev, res.data]);
+            })
+            .catch((err) => {});
+    };
 
     const handleLike = (movie) => {
         if (clicked) return;
 
         setClicked(true);
 
-        UserService.like(user.id, movie.id).then((res) => {
-            setSwipeDirection("right");
-            setClicked(false);
-        });
+        UserService.like(user.id, movie.id)
+            .then((res) => {
+                setSwipeDirection("right");
+                setClicked(false);
+            })
+            .catch((err) => {
+                setClicked(false);
+            });
+
+        loadNextMovie();
     };
 
     const handleDislike = (movie) => {
@@ -140,10 +73,16 @@ const Home = () => {
 
         setClicked(true);
 
-        UserService.dislike(user.id, movie.id).then((res) => {
-            setSwipeDirection("left");
-            setClicked(false);
-        });
+        UserService.dislike(user.id, movie.id)
+            .then((res) => {
+                setSwipeDirection("left");
+                setClicked(false);
+            })
+            .catch((err) => {
+                setClicked(false);
+            });
+
+        loadNextMovie();
     };
 
     const handleWatchLater = (movie) => {
@@ -151,10 +90,16 @@ const Home = () => {
 
         setClicked(true);
 
-        UserService.watchLater(user.id, movie.id).then((res) => {
-            setSwipeDirection("up");
-            setClicked(false);
-        });
+        UserService.watchLater(user.id, movie.id)
+            .then((res) => {
+                setSwipeDirection("up");
+                setClicked(false);
+            })
+            .catch((err) => {
+                setClicked(false);
+            });
+
+        loadNextMovie();
     };
 
     const handleInfo = (movie) => {
@@ -171,24 +116,47 @@ const Home = () => {
         if (!["up", "left", "right"].includes(swipeDirection) || clicked) return;
 
         setSwipeDirection(null);
-        setMovieOver(movieUnder);
+        setMovies((prev) => prev.slice(1));
     };
-
-    if (loading) {
-        return <Spinner />;
-    }
 
     return (
         <Container>
-            <Container>
-                <p>User: {JSON.stringify(user, null, 4)}</p>
-            </Container>
+            <Container className="d-flex justify-content-center my-2">
+                {loading && (
+                    <Container>
+                        <EmptyCard
+                            title={failedToLoad ? "Failed to load..." : <Spinner />}
+                            subtitle={
+                                failedToLoad ? (
+                                    <Button
+                                        onClick={() => {
+                                            loadNextMovie();
+                                        }}
+                                        size="sm"
+                                    >
+                                        Try again?
+                                    </Button>
+                                ) : (
+                                    "Loading..."
+                                )
+                            }
+                        />
+                    </Container>
+                )}
 
-            <Container className="d-flex justify-content-center">
-                {movieUnder && (
+                {movies.length === 0 && !loading && (
+                    <Container>
+                        <EmptyCard
+                            title="You've ran out of movies!"
+                            subtitle={`Try ${(<Button>fixing</Button>)} the criteria`}
+                        ></EmptyCard>
+                    </Container>
+                )}
+
+                {movies.length > 1 && (
                     <div className="under">
                         <MovieCard
-                            movie={movieUnder}
+                            movie={movies[1]}
                             onLike={() => {}}
                             onDislike={() => {}}
                             onWatchLater={() => {}}
@@ -197,16 +165,23 @@ const Home = () => {
                     </div>
                 )}
 
-                <div className={`over ${swipeDirection ? swipeDirection : ""}`} onTransitionEnd={handleTransitionEnd}>
-                    <MovieCard
-                        movie={movieOver}
-                        onLike={handleLike}
-                        onDislike={handleDislike}
-                        onWatchLater={handleWatchLater}
-                        onInfo={handleInfo}
-                    />
-                </div>
-                <InfoModal show={showModal} movie={modalData} onClose={handleModalClose} />
+                {movies.length > 0 && (
+                    <>
+                        <div
+                            className={`over ${swipeDirection ? swipeDirection : ""}`}
+                            onTransitionEnd={handleTransitionEnd}
+                        >
+                            <MovieCard
+                                movie={movies[0]}
+                                onLike={handleLike}
+                                onDislike={handleDislike}
+                                onWatchLater={handleWatchLater}
+                                onInfo={handleInfo}
+                            />
+                        </div>
+                        <InfoModal show={showModal} movie={modalData} onClose={handleModalClose} />
+                    </>
+                )}
             </Container>
         </Container>
     );
