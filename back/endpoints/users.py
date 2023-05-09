b9c629a38c875af8e8b8988a344849cc1fc2ba54
Robin <robertmokanek@gmail.com>
2023-05-09 14:31:51 +0200

    Handling settings at backend

--- back/endpoints/users.py
+++ back/endpoints/users.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request, current_app
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from models import User, Movie, db
+from models import User, Movie, db, Genre, Config
+from json import loads
 
 users_blueprint = Blueprint("users", __name__)
 
@@ -59,3 +60,26 @@ def watch_later(user_id):
     db.session.commit()
 
     return jsonify({"message": "Success"}), 200
+
+
+@users_blueprint.route("<int:user_id>/settings", methods=["POST"])
+@jwt_required()
+def update_settings(user_id):
+    user = User.query.get_or_404(user_id)
+    data = request.form.to_dict()
+
+    names = loads(data.get("excluded_genres", []))
+    genres = Genre.query.filter(Genre.name.in_(names)).all()
+    if genres:
+        user.excluded_genres = genres
+        db.session.commit()
+
+    year_from = data.get("year_from", None)
+    if year_from and year_from < Config.MIN_YEAR_FROM:
+        user.year_from = year_from
+
+    year_to = data.get("year_to", None)
+    if year_to and year_to > Config.MIN_YEAR_TO:
+        user.year_to = year_to
+
+    return jsonify(user.to_dict()), 200
