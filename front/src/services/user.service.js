7a1f431a39cb88f9b715708706718ad5b166bdf3
Robin <robertmokanek@gmail.com>
2023-05-09 14:32:42 +0200

    Updating settings at user service

--- front/src/services/user.service.js
+++ front/src/services/user.service.js
@@ -28,11 +28,22 @@ const watchLater = (user_id, movie_id) => {
     return api.post(`/users/${user_id}/watch-later`, data);
 };
 
+const updateSettings = (user_id, settings) => {
+    const data = new FormData();
+
+    for (const [key, value] of Object.entries(settings)) {
+        data.append(key, JSON.stringify(value));
+    }
+
+    return api.post(`/users/${user_id}/settings`, data);
+};
+
 const UserService = {
     read,
     like,
     dislike,
     watchLater,
+    updateSettings,
 };
 
 export default UserService;
