a93b52bc6e2f67e605c9500e13aecbb15d861011
Robin <robertmokanek@gmail.com>
2023-05-09 20:22:08 +0200

    Fix for missing shadow on EmptyCard

--- front/src/components/EmptyCard/EmptyCard.js
+++ front/src/components/EmptyCard/EmptyCard.js
@@ -5,7 +5,7 @@ import "./EmptyCard.css";
 const EmptyCard = ({ title, subtitle }) => {
     return (
         <Col col={12}>
-            <Card className="empty-card">
+            <Card className="empty-card shadow">
                 <Card.Body className="d-flex flex-column align-items-center justify-content-center">
                     <Card.Title className="title text-center">{title}</Card.Title>
                     <Card.Subtitle className="text-center">{subtitle}</Card.Subtitle>
