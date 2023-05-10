c73ba75533925e548004454a4d9fb7e9d69872af
Robin <robertmokanek@gmail.com>
2023-05-10 16:23:56 +0200

    Updated nav to look better

--- front/src/components/CustomNav.js
+++ front/src/components/CustomNav.js
@@ -5,17 +5,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { removeAuthCookiesAndHeader } from "../services/api";
 import { SET_USER } from "../reducers/types";
-import SettingsModal from "./SettingsModal/SettingsModal";
 import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCog } from "@fortawesome/free-solid-svg-icons";
+import SettingsModal from "./SettingsModal/SettingsModal";
 
 const NavLoggedIn = () => {
     const { user } = useSelector((state) => state.user);
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
     const [showSettings, setShowSettings] = useState(false);
 
+    const handleClickHome = () => navigate("/");
+    const handleClickNewMovie = () => navigate("/admin/new");
+    const handleClickSwipe = () => navigate("/");
+    const handleClickProfile = () => navigate("/profile");
+    const handleClickSettings = () => setShowSettings(true);
+    const handleCloseSettings = () => setShowSettings(false);
     const handleClickLogOut = () => {
         removeAuthCookiesAndHeader();
         dispatch({
@@ -24,46 +29,30 @@ const NavLoggedIn = () => {
         });
     };
 
-    const handleClickHome = () => navigate("/");
-    const handleClickNewMovie = () => navigate("/admin/new");
-    const handleClickSwipe = () => navigate("/");
-
     return (
-        <Navbar bg="dark" variant="dark" expand="md" className="mb-3">
-            <Container>
-                <Navbar.Brand onClick={handleClickHome}>Moviender</Navbar.Brand>
+        <>
+            <Navbar bg="dark" variant="dark" expand="md" className="mb-3">
+                <Container>
+                    <Navbar.Brand onClick={handleClickHome}>Moviender</Navbar.Brand>
 
-                <Navbar.Toggle aria-controls="navbar-nav" />
-                <Navbar.Collapse id="navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link onClick={handleClickSwipe}>Swipe</Nav.Link>
-                        {user?.is_admin && <Nav.Link onClick={handleClickNewMovie}>New Movie</Nav.Link>}
-                    </Nav>
+                    <Navbar.Toggle aria-controls="navbar-nav" />
+                    <Navbar.Collapse id="navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link onClick={handleClickSwipe}>Swipe</Nav.Link>
+                            {user?.is_admin && <Nav.Link onClick={handleClickNewMovie}>New Movie</Nav.Link>}
+                        </Nav>
 
-                    <Nav className="justify-content-end">
-                        <Nav.Link>
-                            <FontAwesomeIcon
-                                icon={faCog}
-                                onClick={() => {
-                                    setShowSettings(true);
-                                }}
-                                color="white"
-                            />
+                        <Nav className="justify-content-end">
+                            <Nav.Link onClick={handleClickProfile}>Profile</Nav.Link>
+                            <Nav.Link onClick={handleClickSettings}>Settings</Nav.Link>
+                            <Nav.Link onClick={handleClickLogOut}>Log out</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
 
-                            {showSettings && (
-                                <SettingsModal
-                                    show={showSettings}
-                                    onClose={() => {
-                                        setShowSettings(false);
-                                    }}
-                                />
-                            )}
-                        </Nav.Link>
-                        <Nav.Link onClick={handleClickLogOut}>Log out</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+            {showSettings && <SettingsModal show={showSettings} onClose={handleCloseSettings} />}
+        </>
     );
 };
 
