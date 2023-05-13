5c7703439f81ae3d925730c6556034d1b67da05c
Robin <robertmokanek@gmail.com>
2023-05-13 14:34:16 +0200

    Piechart to Doughnut chart + new colors

--- front/src/components/Profile/Profile.js
+++ front/src/components/Profile/Profile.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Pie } from "react-chartjs-2";
+import { Doughnut } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
@@ -8,18 +8,49 @@ import CenteredSpinner from "../CenteredSpinner";
 import MovieService from "../../services/movie.service";
 import { Container } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
+
+const ChartBackgroundColors = [
+    "rgba(255, 99, 132, 0.2)",
+    "rgba(54, 162, 235, 0.2)",
+    "rgba(255, 206, 86, 0.2)",
+    "rgba(75, 192, 192, 0.2)",
+    "rgba(153, 102, 255, 0.2)",
+    "rgba(255, 159, 64, 0.2)",
+    "rgba(255, 99, 255, 0.2)",
+    "rgba(0, 153, 153, 0.2)",
+    "rgba(255, 204, 204, 0.2)",
+    "rgba(102, 153, 0, 0.2)",
+    "rgba(255, 215, 0, 0.2)",
+    "rgba(128, 0, 128, 0.2)",
+    "rgba(255, 128, 128, 0.2)",
+    "rgba(0, 128, 128, 0.2)",
+    "rgba(165, 42, 42, 0.2)",
+];
+
+const ChartBorderColors = [
+    "rgba(255, 99, 132, 1)",
+    "rgba(54, 162, 235, 1)",
+    "rgba(255, 206, 86, 1)",
+    "rgba(75, 192, 192, 1)",
+    "rgba(153, 102, 255, 1)",
+    "rgba(255, 159, 64, 1)",
+    "rgba(255, 99, 255, 1)",
+    "rgba(0, 153, 153, 1)",
+    "rgba(255, 204, 204, 1)",
+    "rgba(102, 153, 0, 1)",
+    "rgba(255, 215, 0, 1)",
+    "rgba(128, 0, 128, 1)",
+    "rgba(255, 128, 128, 1)",
+    "rgba(0, 128, 128, 1)",
+    "rgba(165, 42, 42, 1)",
+];
 
 const Profile = ({ show, onClose }) => {
     const { user } = useSelector((state) => state.user);
 
     const [loading, setLoading] = useState(false);
-    const [stats, setStats] = useState({
-        liked_in_genre: [],
-    });
-
     const [data, setData] = useState(null);
 
     useEffect(() => {
@@ -41,14 +72,13 @@ const Profile = ({ show, onClose }) => {
                     });
                 });
 
-                setStats({ liked_in_genre: newStats });
                 setData({
                     labels: Object.keys(newStats),
                     datasets: [
                         {
-                            label: "no. movies",
-                            backgroundColor: "rgba(120, 100, 100, 0.6)",
-                            borderColor: "rgba(0, 0, 0, 0.1)",
+                            label: "# of liked movies",
+                            backgroundColor: ChartBackgroundColors,
+                            borderColor: ChartBorderColors,
                             data: Object.values(newStats),
                         },
                     ],
@@ -69,14 +99,14 @@ const Profile = ({ show, onClose }) => {
     return (
         <Modal show={show} onHide={onClose} size="md">
             <Modal.Header closeButton>
-                <Modal.Title>Settings</Modal.Title>
+                <Modal.Title>Profile</Modal.Title>
             </Modal.Header>
 
             <Modal.Body>
                 <Row>
-                    <Container className="entry-title">Liked movies by genre</Container>
+                    <Container className="entry-title mb-2">Liked movies by genre</Container>
                     <Container className="entry-value">
-                        {data && <Pie data={data} options={{ responsive: true }} />}
+                        {data && <Doughnut data={data} options={{ responsive: true }} />}
                     </Container>
                 </Row>
             </Modal.Body>
